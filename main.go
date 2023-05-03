fa5e8efa918a6d644c7135cfd12a7144bb379e2e
Stanislav Pyatnicyn <pyatnicyn.stanislav@gmail.com>
2023-05-04 01:12:37 +0300

    Implemented grpc server in separated file

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
-
-	"github.com/StasJDM/go-grpc-example/pkg/server/post"
-	"github.com/StasJDM/go-grpc-example/pkg/server/project"
-	"github.com/StasJDM/go-grpc-example/pkg/server/user"
-	postServer "github.com/StasJDM/go-grpc-example/server/post"
-	projectServer "github.com/StasJDM/go-grpc-example/server/project"
-	userServer "github.com/StasJDM/go-grpc-example/server/user"
-	"google.golang.org/grpc"
+	"github.com/StasJDM/go-grpc-example/server"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":9000")
-	if err != nil {
-		log.Fatalf("Listener creation error: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	userService := &userServer.UserServer{}
-	postService := &postServer.PostServer{}
-	projectService := &projectServer.ProjectServer{}
-
-	user.RegisterUserServer(grpcServer, userService)
-	post.RegisterPostServer(grpcServer, postService)
-	project.RegisterProjectServer(grpcServer, projectService)
-
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Failed to serve gRPC: %v", err)
-	}
+	server.Run()
 }
---
+++ server/server.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	postPkg "github.com/StasJDM/go-grpc-example/pkg/server/post"
+	projectPkg "github.com/StasJDM/go-grpc-example/pkg/server/project"
+	userPkg "github.com/StasJDM/go-grpc-example/pkg/server/user"
+
+	"github.com/StasJDM/go-grpc-example/server/post"
+	"github.com/StasJDM/go-grpc-example/server/project"
+	"github.com/StasJDM/go-grpc-example/server/user"
+	"google.golang.org/grpc"
+)
+
+type AppServiceSet struct {
+	UserService    *user.UserServer
+	PostService    *post.PostServer
+	ProjectService *project.ProjectServer
+}
+
+func Run() {
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Listener creation error: %v", err)
+	}
+
+	serviceSet := newAppServiceSet()
+	grpcServer := newGrpcServer(serviceSet)
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
+}
+
+func newAppServiceSet() *AppServiceSet {
+	userService := &user.UserServer{}
+	postService := &post.PostServer{}
+	projectService := &project.ProjectServer{}
+
+	return &AppServiceSet{
+		UserService:    userService,
+		PostService:    postService,
+		ProjectService: projectService,
+	}
+}
+
+func newGrpcServer(serviceSet *AppServiceSet) *grpc.Server {
+	grpcServer := grpc.NewServer()
+
+	userPkg.RegisterUserServer(grpcServer, serviceSet.UserService)
+	postPkg.RegisterPostServer(grpcServer, serviceSet.PostService)
+	projectPkg.RegisterProjectServer(grpcServer, serviceSet.ProjectService)
+
+	return grpcServer
+}
